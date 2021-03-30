363d0f81ea956ddf5c40a0a8166f783e8e3c0e0d
Natasha Szulczewski <natashaszulczewski@Natashas-MacBook-Pro.local>
2021-03-30 01:20:06 -0500

    maybe last commit didnt work, its here on my machine?

--- client/resources/scripts/index.js
+++ client/resources/scripts/index.js
@@ -18,3 +18,79 @@ function getPosts()
         console.log(error);
     });
 }
+
+function addPost()
+{
+    const postApiUrl = "https://localhost:5001/api/posts";
+    const postAdd = document.getElementById("text").postText;
+    
+    fetch(postApiUrl, 
+    {
+        method: "POST",
+        headers: 
+        {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(
+        {
+            text: postAdd
+        })
+    })
+    .then((response)=>
+    {
+        console.log(response);
+        getPosts();
+    })
+}
+
+function editPost()
+{
+    const editApiUrl = "https://localhost:5001/api/posts";
+    const postEdit = document.getElementById("text").setAttribute('onclick', 'addPost('+text+')');
+    
+    fetch(editApiUrl, 
+    {
+        method: "PUT",
+        headers: 
+        {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(
+        {
+            text: postEdit
+        })
+    })
+    .then((response)=>
+    {
+        console.log(response);
+        getPosts();
+    })
+}
+
+function deletePost()
+{
+    const deleteApiUrl = "https://localhost:5001/api/posts";
+    const postDelete = document.getElementById("id").value;
+    
+    fetch(deleteApiUrl, 
+    {
+        method: "DELETE",
+        headers: 
+        {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(
+        {
+            id: postDelete
+        })
+    })
+    .then((response)=>
+    {
+        console.log(response);
+        getPosts();
+    })
+}
+
